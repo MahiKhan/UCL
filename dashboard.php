d08b23fd6edc1df1c72e92aa444953cdf6606ac9
Mahi Khan <Mahi@Mahis-MacBook-Pro.local>
2013-11-19 12:23:11 +0000

    first commit

---
+++ dashboard.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">  <!-- Above Needed? Lets browser now what type of document/html. If not specified, will use 'quirks mode' - a general format to render which might not do it properly--> 
+<head>
+	<title> Dashboard </title>
+	<meta charset="utf-8"> <!-- Use of this? Ensures default encoding, not sure if 100% necessary. HTML5 slides say 100% necessary. Closes UTF-7 security hole. --> 
+	<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+	<nav class = "navbar"> 
+	
+	<ul>
+		<li> Home </li>
+	</ul>
+	
+	</nav> 
+
+	<div class = "introBackground">
+	<section id = "intro">
+		<h1> About Me </h1>
+		<article> Text from fb. Other BS. Filler. Text from fb. Other BS. Filler. Text from fb. Other BS. Filler. Text from fb. Other BS. Filler.
+		<?php // Include api file here ?>
+			
+		</article>
+		
+		
+		
+		<h2> Project </h2>
+		<article>  
+		Inline text 
+		</article>	
+	</section> 
+	</div> 
+
+	<section id ="wordpress">
+		<h1> Updates </h1>
+		<iframe width="420" height="315" src="http://numbugs.wordpress.com/" frameborder="0" allowfullscreen></iframe>
+	</section>
+
+</section> 
+
+
+<section id ="github">
+
+</section> 
+
+
+</body>
